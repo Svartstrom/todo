90798b1bcfaf33e7b8763200ab4edf24fc982b7d
john eriksson <johne109@gmail.com>
2023-02-27 12:30:40 +0100

    added print argv to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+use std::env;
+
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("TODO")
+        .unwrap();
+
+    let mut full = String::new();
+    for argument in env::args().skip(1) {
+        full = full + &argument + " ";
+    }
+    if let Err(e) = writeln!(file, "{}",full) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
 }
